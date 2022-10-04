1363138602826751536dbeb90edc6828f3da231f
belenher <101005436+belenher@users.noreply.github.com>
2022-10-04 18:21:20 -0300

    creación del security package

--- src/main/java/com/portfolio/Belenher/Controller/PersonaController.java
+++ src/main/java/com/portfolio/Belenher/Controller/PersonaController.java
@@ -5,6 +5,7 @@ import com.portfolio.Belenher.Entity.Persona;
 import com.portfolio.Belenher.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -26,17 +27,20 @@ public class PersonaController {
         return ipersonaService.getPersona();
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona){
         ipersonaService.savePersona(persona);
         return "La persona fue creada correctamente";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id) {
         ipersonaService.deltePersona(id);
         return "La persona fue eliminada correctamente";
 }
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
                                @RequestParam("nombre") String nuevoNombre,
